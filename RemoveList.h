c6d68a3712d8cbaa56cd28592b5a3f303425866e
Yahwon <37835539+Ya-hwon@users.noreply.github.com>
2021-11-20 14:45:54 +0100

    resizable RemoveList + conversion to array
    # RemoveList now optinally resizable
    + conversion to primitive array of elem

--- RemoveList.h
+++ RemoveList.h
@@ -7,7 +7,7 @@
 
 namespace J {
 
-	template<typename T>
+	template<typename T, bool resizable = false>
 	class RemoveList {
 
 	private:
@@ -15,33 +15,50 @@ namespace J {
 			T _data;
 			elem* next;
 		};
-		elem* const _data;
+		elem* _data;
 		elem* first;
-		const size_t size;
-		size_t index = 0;
+		size_t allocSize;
+		size_t _size;
+		size_t writeIndex;
 	public:
 		~RemoveList() {
 			delete[] _data;
 		}
-		RemoveList(const size_t& size) :size(size), _data(new elem[size]), first(_data) {
+		RemoveList(const size_t& size) :allocSize(size), _size(0), writeIndex(0), _data(new elem[size]), first(_data) {
 
 		}
 		void add(const T& value) {
-			if (index < size) {
-				_data[index].next = (index == size - 1) ? nullptr : (_data + index + 1);
-				_data[index++]._data = value;
+			if (resizable && writeIndex == allocSize) {
+				allocSize *= 2;
+				elem* new_data = new elem[allocSize];
+				size_t index = 0;
+				for (elem* current = first; current != nullptr; current = current->next, index++) {
+					new_data[index]._data = current->_data;
+					new_data[index].next = new_data + index + 1;
+				}
+				delete[] _data;
+				_data = new_data;
+				first = new_data;
 			}
+			_data[writeIndex].next = (writeIndex == allocSize - 1) ? nullptr : (_data + writeIndex + 1);
+			_data[writeIndex++]._data = value;
+			_size++;
 		}
 		size_t removeIf(const std::function<bool(const T&)>& filter) {
 			size_t count = 0;
 			elem* current = first;
-			if (filter(current->_data)) first = current->next;
+			if (filter(current->_data)) {
+				first = current->next;
+				_size--;
+				count++;
+			}
 			elem* previous = current;
 			current = current->next;
 			for (; current != nullptr; previous = current, current = current->next) {
 				if (filter(current->_data)) {
 					previous->next = current->next;
 					count++;
+					_size--;
 				}
 			}
 			return count;
@@ -49,6 +66,19 @@ namespace J {
 		void forEach(const std::function<void(T&)> function) {
 			for (elem* current = first; current != nullptr; current = current->next) {
 				function(current->_data);
+			}
+		}
+		size_t capacity() const {
+			return allocSize;
+		}
+		size_t size() const {
+			return _size;
+		}
+		[[nodiscard]] T* toArray() const {
+			T* outArray = new T[_size];
+			size_t i = 0;
+			for (elem* current = first; i < _size; current = current->next, i++) {
+				outArray[i] = current->_data;
 			}
 		}
 	};
